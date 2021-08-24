32a91e7f92dd20482b1efa375b5d684a6c135797
MariaMednova <maeria.guseynova@gmail.com>
2021-08-24 13:17:58 +0300

    Maven realisation Task 10

---
+++ src/main/java/casino_new/Сasinoo.java
@@ -0,0 +1,11 @@
+package casino_new;
+
+import java.util.Scanner;
+
+public class Сasinoo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        System.out.println(userInput);
+    }
+}
